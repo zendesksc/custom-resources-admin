e5f551a2db440db757c4175bf04bcf15c9b8fa1b
Adam Gray <adam@pateo.co>
2018-05-05 15:23:45 +0100

    Add ability to disable text field

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const TextField = ({ label, name, value, error, onChange }) => (
+const TextField = ({ label, name, value, error, onChange, disabled }) => (
   <div className='form-group'>
     <label>{label}</label>
-    <input className={error ? 'form-control is-invalid' : 'form-control'} type='text' name={name} value={value} onChange={onChange} />
+    <input className={error ? 'form-control is-invalid' : 'form-control'} type='text' name={name} value={value} onChange={onChange} disabled={disabled} />
     <div className="invalid-feedback">
       {error}
     </div>

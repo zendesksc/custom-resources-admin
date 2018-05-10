c37b3fe4c3655e005f70943753e9eb776a90ae9b
Adam Gray <adam@pateo.co>
2018-05-10 11:40:04 +0100

    Failed an experiement

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 process.env.NODE_ENV = 'production'
 
-var reactScriptsConfig = require('react-scripts/config/webpack.config.prod')
+const reactScriptsConfig = require('react-scripts/config/webpack.config.prod')
 
 module.exports = Object.assign({}, reactScriptsConfig,
   {

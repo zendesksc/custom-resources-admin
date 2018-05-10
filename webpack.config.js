6b80a01ab61598b8b6b0f2a8fb7509781f0dd9c8
Adam Gray <adam@pateo.co>
2018-05-10 09:59:35 +0100

    Connect react app to zat

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// https://github.com/facebook/create-react-app/issues/1354
+
+process.env.NODE_ENV = 'production'
+
+var reactScriptsConfig = require('react-scripts/config/webpack.config.prod')
+
+module.exports = Object.assign({}, reactScriptsConfig, {
+  output: Object.assign({}, reactScriptsConfig.output, {
+    path: __dirname + '/dist/assets'
+  })
+})

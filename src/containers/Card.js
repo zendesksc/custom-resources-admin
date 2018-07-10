d6a5f2e41e2cac838a6ef0804292e40c030cfc11
Adam Gray <adam@pateo.co>
2018-07-10 15:30:44 +0100

    Refresh codebase by initialising again with create-react-app

---
+++ src/containers/Card.js
@@ -0,0 +1,163 @@
+import React, { Component } from 'react'
+import NewResourceTypeForm from './NewResourceTypeForm';
+import ResourcesList from './ResourcesList';
+import ErrorMessage from '../components/ErrorMessage';
+
+const MODES = {
+  NEW_RESOURCE_TYPE_FORM: 0,
+  RESOURCES_LIST: 1
+}
+
+class Card extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasError: false,
+      errors: [],
+      mode: MODES.NEW_RESOURCE_TYPE_FORM,
+      resourceType: {}
+    }
+
+    this.handleNewResourceTypeFormSuccess = this.handleNewResourceTypeFormSuccess.bind(this)
+    this.handleDeleteResourceType = this.handleDeleteResourceType.bind(this)
+    this.handleNewError = this.handleNewError.bind(this)
+  }
+
+  componentDidMount() {
+    if (this.props.resourceType !== undefined) {
+      this.setState({
+        mode: MODES.RESOURCES_LIST,
+        resourceType: this.formatResourceType(this.props.resourceType)
+      })
+    }
+  }
+
+  formatResourceType(resourceType) {
+    // We've formatted the resourceType in a particular way that makes it easier to work
+    // with the data in React. This function basically converts the schema.properties
+    // into an array that can be mapped over in ResourcesList.
+
+    let fields = []
+    let rawProperties = resourceType.schema.properties
+
+    for (let key in rawProperties) {
+      fields.push({
+        name: key,
+        type: rawProperties[key].type,
+        description: rawProperties[key].description
+      })
+    }
+
+    return {
+      title: resourceType.key,
+      key: resourceType.key,
+      fields: fields
+    }
+
+  }
+
+  handleNewResourceTypeFormSuccess(submittedResourceType) {
+    this.setState({
+      hasError: false,
+      errors: [],
+      resourceType: this.formatResourceType(submittedResourceType)
+    })
+    this.props.onNewResourceTypeSuccess(submittedResourceType)
+  }
+
+  handleDeleteResourceType() {
+    let key = this.state.resourceType.key
+
+    window.client.request({
+      url: '/api/custom_resources/resource_types/' + key,
+      type: 'DELETE'
+    })
+      .then((res) => {
+        // Define each relationship type name
+        let relationshipTypes = ['users', 'tickets', 'organizations']
+
+        // When we delete a resource type, we need to delete all associated relationship types.
+        let relationshipTypesPromises = []
+
+        relationshipTypes.forEach((type) => {
+          relationshipTypesPromises.push(
+            window.client.request({
+              url: '/api/custom_resources/relationship_types/' + type + '_' + key + 's',
+              type: 'DELETE'
+            })
+          )
+        })
+
+        return Promise.all(relationshipTypesPromises)
+      })
+      .then((res) => this.props.onDelete(key))
+      .catch((err) => {
+        this.setState({
+          hasError: true,
+          errors: err.responseJSON.errors
+        })
+      })
+  }
+
+  handleNewError(errors) {
+    this.setState({
+      hasError: true,
+      errors
+    })
+  }
+
+  render() {
+    if (this.state.mode === MODES.NEW_RESOURCE_TYPE_FORM) {
+      return (
+        <div className='card'>
+          <div className='card-header'>
+            <div className='row'>
+              <div className='col-6'>
+                <h4 className='mb-0'>New Resource Type</h4>
+              </div>
+            </div>
+          </div>
+          <div className='card-body'>
+            {this.state.hasError ? <ErrorMessage errors={this.state.errors} /> : null}
+            <NewResourceTypeForm
+              onError={this.handleNewError}
+              onSuccess={this.handleNewResourceTypeFormSuccess} />
+          </div>
+        </div>
+      )
+    }
+
+    if (this.state.mode === MODES.RESOURCES_LIST) {
+      return (
+        <div className='card mb-4'>
+          <div className='card-header'>
+            <div className='row'>
+              <div className='col-6' data-toggle="collapse" data-target={'#' + this.state.resourceType.key}>
+                <h4 className='mb-0'>{this.state.resourceType.key}</h4>
+              </div>
+              <div className='col-6'>
+                <button
+                  className='btn btn-outline-danger float-right'
+                  onClick={this.handleDeleteResourceType}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='collapse show' id={this.state.resourceType.key}>
+            <div className='card-body'>
+              {this.state.hasError ? <ErrorMessage errors={this.state.errors} /> : null}
+              <ResourcesList
+                onError={this.handleNewError}
+                resourceType={this.state.resourceType} />
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+  }
+}
+
+export default Card
---
+++ src/containers/ResourceTypeList.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import Card from './Card'
+
+class ResourceTypeList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoading: false,
+      resourceTypes: []
+    }
+
+    this.addSubmittedResourceTypeToState = this.addSubmittedResourceTypeToState.bind(this)
+    this.removeResourceTypeFromState = this.removeResourceTypeFromState.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true
+    })
+
+    // Fetch resource types from API and set to state
+    window.client.request({
+      url: '/api/custom_resources/resource_types',
+      type: 'GET'
+    }).then((res) => {
+      this.setState({
+        isLoading: false,
+        resourceTypes: res.data
+      })
+    })
+      .catch((err) => console.log(err))
+  }
+
+  addSubmittedResourceTypeToState(resourceType) {
+    this.setState({
+      resourceTypes: this.state.resourceTypes.concat(resourceType)
+    })
+  }
+
+  removeResourceTypeFromState(key) {
+    this.setState({
+      resourceTypes: this.state.resourceTypes.filter((resourceType) => resourceType.key !== key)
+    })
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )
+    }
+
+    let resourceTypes = this.state.resourceTypes.map((resourceType, index) =>
+      <Card
+        key={resourceType.key}
+        resourceType={resourceType}
+        onDelete={this.removeResourceTypeFromState}
+      />
+    )
+
+    return (
+      <div>
+        {resourceTypes}
+
+        <Card onNewResourceTypeSuccess={this.addSubmittedResourceTypeToState} />
+      </div>
+    )
+  }
+}
+
+export default ResourceTypeList
